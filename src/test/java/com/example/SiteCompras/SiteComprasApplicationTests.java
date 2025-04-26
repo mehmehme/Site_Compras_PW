cfe8dbf790c520d76d30828d369b679e47dee9ee
unknown <luiscff2001@hotmail.com>
2025-04-26 15:31:15 -0300

    Adiciona o diretório src/test/ ao controle de versão

---
+++ src/test/java/com/example/SiteCompras/SiteComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SiteCompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
